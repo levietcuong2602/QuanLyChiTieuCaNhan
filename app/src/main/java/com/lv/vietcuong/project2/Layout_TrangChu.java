c864ef95a56010c1c4c0f5ae64ebd0aa5c184dd4
VietCuong <levietcuong0297@gmail.com>
2018-03-25 09:44:52 +0700

    them fragment profile

--- app/src/main/java/com/lv/vietcuong/project2/Layout_TrangChu.java
+++ app/src/main/java/com/lv/vietcuong/project2/Layout_TrangChu.java
@@ -3,6 +3,9 @@ package com.lv.vietcuong.project2;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,11 +13,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.lv.vietcuong.project2.Model.Account;
 import com.lv.vietcuong.project2.View.DangNhap.DangNhapActivity;
+import com.lv.vietcuong.project2.View.DangNhap.Profile.Fragment_DoiAvata;
+import com.lv.vietcuong.project2.View.DangNhap.Profile.Fragment_DoiMatKhau;
+import com.lv.vietcuong.project2.View.DangNhap.Profile.Fragment_DoiTenTaiKhoan;
 
 public class Layout_TrangChu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -22,7 +31,7 @@ public class Layout_TrangChu extends AppCompatActivity implements NavigationView
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     TextView textName;
-
+    FragmentManager manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,9 +41,7 @@ public class Layout_TrangChu extends AppCompatActivity implements NavigationView
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigationView);
 
-        View headerLayout = navigationView.inflateHeaderView(R.layout.layout_header_profile);
-        textName = headerLayout.findViewById(R.id.tvName);
-        textName.setText("haha");
+        setHeaderNavigation();
 
         setSupportActionBar(toolbar);
 
@@ -53,15 +60,45 @@ public class Layout_TrangChu extends AppCompatActivity implements NavigationView
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    private void setHeaderNavigation() {
+        View headerLayout = navigationView.inflateHeaderView(R.layout.layout_header_profile);
+        textName = headerLayout.findViewById(R.id.tvName);
+
+        Account account = (Account) getIntent().getSerializableExtra("account");
+        textName.setText("Xin chào: "+account.getUsername());
+
+        manager = getSupportFragmentManager();
+
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         switch (id){
-            case R.id.itemDoiTaiKhoan:
+            case R.id.itemDoiTenTaiKhoan:
+                FragmentTransaction transDoiTenTK = manager.beginTransaction();
+
+                Fragment_DoiTenTaiKhoan fragmentDoiTenTK = new Fragment_DoiTenTaiKhoan();
+                transDoiTenTK.replace(R.id.content_layout, fragmentDoiTenTK);
+                transDoiTenTK.commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
                 break;
             case R.id.itemDoiMatKhau:
+                FragmentTransaction transaction = manager.beginTransaction();
+
+                Fragment_DoiMatKhau fragmentDoiMK = new Fragment_DoiMatKhau();
+                transaction.replace(R.id.content_layout, fragmentDoiMK);
+                transaction.commit();
+
+                drawerLayout.closeDrawer(GravityCompat.START);
                 break;
             case R.id.itemDoiAvata:
+                FragmentTransaction transDoiAvata = manager.beginTransaction();
+
+                Fragment_DoiAvata fragmentDoiAvata = new Fragment_DoiAvata();
+                transDoiAvata.replace(R.id.content_layout, fragmentDoiAvata);
+                transDoiAvata.commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
                 break;
             case R.id.itemDangXuat:
                 Intent intent = new Intent(Layout_TrangChu.this, DangNhapActivity.class);
---
+++ app/src/main/java/com/lv/vietcuong/project2/View/DangNhap/Profile/Fragment_DoiTenTaiKhoan.java
@@ -0,0 +1,23 @@
+package com.lv.vietcuong.project2.View.DangNhap.Profile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lv.vietcuong.project2.R;
+
+/**
+ * Created by Administor on 3/25/2018.
+ */
+
+public class Fragment_DoiTenTaiKhoan extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_fragment_doitentaikhoan, container, false);
+        return view;
+    }
+}
